c9524cd61bc70f0484e6ec9bf368c642ac817d1d
Piero Dotti <progiemmeh@gmail.com>
2025-10-28 15:09:55 +0100

    fix(asdf): handle amd64 architecture

--- .devcontainer/local-features/asdf/install.sh
+++ .devcontainer/local-features/asdf/install.sh
@@ -3,9 +3,25 @@ set -e
 
 ASDF_VERSION=${ASDF_VERSION:-"0.18.0"}
 
-sudo wget https://github.com/asdf-vm/asdf/releases/download/v$ASDF_VERSION/asdf-v$ASDF_VERSION-linux-arm64.tar.gz
-sudo tar -xzf asdf-v$ASDF_VERSION-linux-arm64.tar.gz -C /usr/local/bin
-sudo rm asdf-v$ASDF_VERSION-linux-arm64.tar.gz
+ARCH=$(uname -m)
+case $ARCH in
+    x86_64|amd64)
+        ASDF_ARCH="linux-amd64"
+        ;;
+    aarch64|arm64)
+        ASDF_ARCH="linux-arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+echo "Installing ASDF version $VERSION for $ASDF_ARCH..."
+
+sudo wget https://github.com/asdf-vm/asdf/releases/download/v$ASDF_VERSION/asdf-v$ASDF_VERSION-$ASDF_ARCH.tar.gz
+sudo tar -xzf asdf-v$ASDF_VERSION-$ASDF_ARCH.tar.gz -C /usr/local/bin
+sudo rm asdf-v$ASDF_VERSION-$ASDF_ARCH.tar.gz
 echo 'export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"' >> ~/.bashrc
 
 export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
